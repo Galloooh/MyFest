b11a8dcf8d9451386d6a96804dd760c0eba8e8a5
Simone <simonesbreglia@gmail.com>
2020-02-21 10:00:14 +0100

    Creazione classe utente, evento, inizio Login

---
+++ InstantFest_2/InstantFest_2/Models/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstantFest_2.Models.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Email address")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        
+
+    }
+}
